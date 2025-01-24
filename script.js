796990b7f2cd3240f3309bb2f27552bc1ce79a1b
ub-victor <victoireushindi371@gmail.com>
2025-01-24 12:49:17 +0200

    Yaalaluah  !!!

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ window.onload = function() {
   var name = "Victor";
   var belt = "Black";
    var victor = {
-    name: name,
-    belt: belt,
+    name , belt,
+    chop(x) {
+      console.log(`You chopped the enemy ${x} times ok`);
+    }
    };
-  console.log(victor.name);
+  console.log(victor.chop(5));
 }

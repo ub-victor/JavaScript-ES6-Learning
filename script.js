1a96e45fea687746277fef29277949bee7cd4c3f
ub-victor <victoireushindi371@gmail.com>
2025-01-24 15:19:47 +0200

    Yaalaluah  !!!

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-var 
+var ninja = {
+  name: "victoire",
+  chop(x){
+    window.setInterval(function(){
+      if(x > 0){
+        console.log(this.name + " chopped the enemy");
+        x--;
+      }
+    },1000);
+  }
+}
+ninja.chop(5);
 }
